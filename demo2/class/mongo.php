bc476796f3bbf59341f3a3b99305bf9f1337b652
datdo <dathalongbay@gmail.com>
2019-09-01 14:39:34 +0700

    111

--- demo2/class/mongo.php
+++ demo2/class/mongo.php
@@ -8,4 +8,51 @@
 
 class Mongo extends Database implements Crud , Log , Search {
 
+
+    public function connect()
+    {
+        // TODO: Implement connect() method.
+        $connection = new MongoClient( "mongodb://example.com:65432" );
+        /// connect to a remote host at a given port
+    }
+
+    public function insert() {
+
+
+    }
+
+    public function create()
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function delete()
+    {
+        // TODO: Implement delete() method.
+    }
+
+    public function read()
+    {
+        // TODO: Implement read() method.
+    }
+
+    public function update()
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function search()
+    {
+
+    }
+
+    public function readLog()
+    {
+        // TODO: Implement readLog() method.
+    }
+
+    public function writeLog()
+    {
+        // TODO: Implement writeLog() method.
+    }
 }
--- demo2/class/mysql.php
+++ demo2/class/mysql.php
@@ -8,4 +8,51 @@
 
 class Mysql extends Database implements Crud , Log , Search {
 
+
+    public function connect()
+    {
+        // TODO: Implement connect() method.
+        $connection = mysqli_connect("", "", "");
+    }
+
+    public function insert() {
+
+
+    }
+
+    public function create()
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function delete()
+    {
+        // TODO: Implement delete() method.
+    }
+
+    public function read()
+    {
+        // TODO: Implement read() method.
+    }
+
+    public function update()
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function search()
+    {
+
+    }
+
+    public function readLog()
+    {
+        // TODO: Implement readLog() method.
+    }
+
+    public function writeLog()
+    {
+        // TODO: Implement writeLog() method.
+    }
+
 }
